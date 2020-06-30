e0b5d2f2da796afbfa010a0758bc72db0560618c
Ricardo Nogueira <ricardo@ifsp.edu.br>
2020-06-30 16:23:35 -0300

    Arquivo com comandos do Javascrcipt

---
+++ meuJS.js
@@ -0,0 +1,33 @@
+/*
+	Declarando variáveis
+	na linguagem Javascript
+*/
+
+var numero1 = parseFloat(    prompt("Digite um valor: ")          );
+var numero2 = parseFloat(    prompt("Digite outro valor: ")        );
+
+// Caixas de Dialogo
+alert('Mensagem')
+var msg = prompt('Entrada de dados') //Retorna String
+var booleano = confirm('Aperte OK ou Cancelar') // Retorna Verdadeiro ou Falso
+
+// Conversores de String para Numéricos
+// parseInt(<String>)
+// parseFloat(String)
+
+//Operadores Matemáticos
+var soma = numero1 + numero2;
+var subtracao = numero1 - numero2;
+var multiplicacao = numero1 * numero2;
+var divisao = numero1 / numero2;
+var incremento = numero1++;
+var decremento = numero1--;
+
+//Operadores Lógicos {true ou false}
+var maior = 5 > 5;
+var menor = 7 < 12;
+var maiorIgual = 67 >= 45;
+var menorIgual = 3 <= 4;
+var igualdade = 5 == 3;
+var diferente = 0 != 9;
+
